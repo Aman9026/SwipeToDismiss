a5f1d3f747683aeba8f5558f0981de27ae2e693e
Aman9026 <amangupta884036@gmail.com>
2020-09-17 09:43:05 +0530

    implemented swipe to dismiss

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: new MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  final List<String> items = List<String>.generate(30, (i) => "Items ${i + 1}");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Swipe to dismiss"),
+      ),
+      body: new ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          // final item = items[index];
+          return new Dismissible(
+            key: new Key(items[index]),
+            onDismissed: (direction) {
+              items.removeAt(index);
+              Scaffold.of(context).showSnackBar(new SnackBar(
+                content: new Text("Item dismissed"),
+              ));
+            },
+            background: new Container(
+              color: Colors.red,
+            ),
+            child: new ListTile(
+              title: new Text("${items[index]}"),
+            ),
+          );
+        },
+      ),
     );
   }
 }
